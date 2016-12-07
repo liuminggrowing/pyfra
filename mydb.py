7b143dde7d8f9cf24a4af90205b8a692ed985e78
liuminggrowing <ec2-user@ip-172-31-5-229.ap-southeast-1.compute.internal>
2016-12-07 06:05:59 +0000

    update

--- mydb.py
+++ mydb.py
@@ -1,23 +1,40 @@
 import sys
 import os
 from mylog import mylog
-from pornhub import pornhub
+import ConfigParser
+import MySQLdb
+import MySQLdb.cursors
 
 class mydb:
     def __init__(self,section):
         self.cf = ConfigParser.ConfigParser()
+        self.cwd = os.getcwd()
         self.cf.read(self.cwd+"/config.conf")
         conf = self.getConf(section) 
-        self.db = MySQLdb.connect(host=conf[0],port=int(conf[1]),user=conf[2],passwd=conf[3],db=conf[4],charset="utf8") 
+        self.db = MySQLdb.connect(host=conf[0],port=int(conf[1]),user=conf[2],passwd=conf[3],db=conf[4],charset="utf8",cursorclass = MySQLdb.cursors.DictCursor) 
+        self.cursor = self.db.cursor()
 
-    def getConfig(self,section):
-        return (cf.get(section,'host'), \
-                cf.get(section,'port'), \
-                cf.get(section,'user'), \
-                cf.get(section,'passwd'), \
-                cf.get(section,'db'))
+    def getConf(self,section):
+        return (self.cf.get(section,'host'), \
+                self.cf.get(section,'port'), \
+                self.cf.get(section,'user'), \
+                self.cf.get(section,'passwd'), \
+                self.cf.get(section,'db'))
 
 
-    def close():
-        self.db.close())
+    def close(self):
+        self.db.close()
+
+    def insert(self,sql):
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+
+    def select(self,sql):
+        self.cursor.execute(sql)
+        ret = self.cursor.fetchall()
+        self.db.commit()
+        return ret
 
--- mylog.py
+++ mylog.py
@@ -7,9 +7,8 @@ class mylog:
         self.cf = ConfigParser.ConfigParser()
         self.cwd = os.getcwd()
         self.cf.read(self.cwd+"/config.conf")
-        self.log_path = self.cf.get('log','path')
         file_name = time.strftime("%Y%m%d",time.localtime())
-        self.log = open(self.log_path + "/log/" + file_name + ".log","a")
+        self.log = open(self.cwd + "/log/" + file_name + ".log","a")
 
     def fwrite(self,msg):
         curTime = time.strftime('%Y-%m-%d %H:%M:%S')
